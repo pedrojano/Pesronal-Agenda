a53ed3e2020efd271989d435b68d0aa0eb85446e
Pedro Jano <pedrino022@gmail.com>
2026-03-19 00:12:59 -0300

    ssl

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,26 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+if (!process.env.DATABASE_URL) {
+  console.warn("Variável DATABASE_URL não encontrada!");
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err, client, release) => {
   if (err) {
     console.error("Erro CRÍTICO ao conectar no Banco de Dados:", err.message);
     console.error(
-      'Dica: Verifique se o banco "personal_agenda_db" foi criado.'
+      'Dica: Verifique se o banco "personal_agenda_db" foi criado e se a DATABASE_URL está correta.',
     );
   } else {
-    console.log("Conexão com o Banco de Dados estabelecida com sucesso.");
+    console.log("Conexão com o Banco de Dados estabelecida com sucesso. 🚀");
     release();
   }
 });
